d488f09fade650dd3ed2155aaaecec43bf9f7742
Conor Dunne <conordunne@outlook.ie>
2023-06-05 18:48:01 +0900

    Add express-session and bcyrpt compare

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const session = require("express-session");
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -7,6 +8,7 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("./models/user")
+const bcrypt = require("bcryptjs");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -32,6 +34,18 @@ db.on("open", () => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+//Middleware
+
+app.use(session({secret: "cats", resave: false, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({extended: false}));
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -53,13 +67,20 @@ passport.use(
     try {
       const user = await User.findOne({ username: username });
       if (!user) {
+        console.log("Incorrect username")
         return done(null, false, { message: "Incorrect username" });
       };
-      if (user.password !== password) {
-        console.log("Wrong password");
-        return done(null, false, { message: "Incorrect password" });
-      };
-      return done(null, user);
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          console.log(`Welcome ${username}`);
+          return done(null, user)
+        } else {
+          // passwords do not match!
+          console.log("Incorrect password")
+          return done(null, false, { message: "Incorrect password" })
+        }
+      })
     } catch(err) {
       return done(err);
     };
